18d46e4a276770b94b376ccc5d9265f420f8325b
Yash Shukla <yshukla2775@gmail.com>
2025-03-22 11:49:40 +0530

    made changes for deploying project on vercel

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,4 +1,5 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/users";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/jobs";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+export const USER_API_END_POINT = `${import.meta.env.VITE_BACKEND_URL}/api/v1/users`;
+export const JOB_API_END_POINT = `${import.meta.env.VITE_BACKEND_URL}/api/v1/jobs`;
+export const COMPANY_API_END_POINT = `${import.meta.env.VITE_BACKEND_URL}/api/v1/company`;
+export const APPLICATION_API_END_POINT = `${import.meta.env.VITE_BACKEND_URL}/api/v1/application`;
+
